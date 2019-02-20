23c48afc80426ad11a6941d371f096d059fba27c
mishu <mishu@xtelecom.ro>
2019-02-20 15:52:46 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# A simple dropdown indicator
+
+[![License](https://poser.pugx.org/laravel-enso/dropdown-indicator/license)](https://packagist.org/packages/laravel-enso/dropdown-indicator)
+[![Latest Stable Version](https://poser.pugx.org/laravel-enso/dropdown-indicator/version)](https://packagist.org/packages/laravel-enso/dropdown-indicator)
+
+A simple dropdown indicator
+
+### Features
+
+- soon
+
+### Configuration & Usage
+
+- soon
+
+### Contributions
+
+are welcome. Pull requests are great, but issues are good too.
+
+### License
+
+This package is released under the MIT license.
---
+++ src/DropdownIndicator.vue
@@ -0,0 +1,56 @@
+<template>
+    <span class="icon angle"
+        :aria-hidden="!expanded">
+        <fa icon="angle-down"/>
+    </span>
+</template>
+
+<script>
+
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faAngleDown } from '@fortawesome/free-solid-svg-icons';
+
+library.add(faAngleDown);
+
+export default {
+    model: {
+        prop: 'collapsed',
+        event: 'toggle',
+    },
+
+    props: {
+        collapsed: {
+            type: Boolean,
+            default: true,
+        },
+    },
+
+    data: v => ({
+        expanded: !v.collapsed,
+    }),
+
+    watch: {
+        collapsed(collapsed) {
+            this.expanded = !collapsed;
+        },
+    },
+
+    methods: {
+        toggle() {
+            this.expanded = !this.expanded;
+            this.$emit('toggle', this.expanded);
+        },
+    },
+};
+
+</script>
+
+<style lang="scss">
+    .icon.angle[aria-hidden="false"] {
+        transform: rotate(180deg);
+    }
+
+    .icon.angle {
+        transition: transform .300s ease;
+    }
+</style>
