fd697e13ac226d7a91d7ccb55edf1fda2b25adc4
Mihai <mihai.ocneanu@gmail.com>
2021-11-22 16:10:27 +0200

    wip

--- src/DropdownIndicator.vue
+++ src/DropdownIndicator.vue
@@ -23,12 +23,6 @@ export default {
             default: false,
         },
     },
-
-    computed: {
-        ariaHidden() {
-            return this.open ? null : true;
-        }
-    }
 };
 </script>
 
