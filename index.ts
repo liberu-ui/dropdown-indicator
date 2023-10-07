061d5e2183fc0066dada31462eb4a44e2c373c2b
Curtis Delicata <curtis.delicata@outlook.com>
2023-10-07 20:35:03 +0000

    Replace JavaScript with TypeScript

---
+++ index.ts
@@ -0,0 +1,8 @@
+/**
+ * This module exports the DropdownIndicator component.
+ * The Dropdown Indicator is a custom Vue component used to display an indicator icon in a dropdown menu.
+ */
+
+import DropdownIndicator from './src/DropdownIndicator.vue';
+
+export default DropdownIndicator;
